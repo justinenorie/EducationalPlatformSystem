43a899e6476904bd3f2e0f5237a7a50b5abc7212
justinenorie <justinedelacruz97@gmail.com>
2023-11-26 23:15:44 +0800

    changes

---
+++ Content/course1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course 1</title>
+    <link rel="stylesheet" href="/IT-ELECT-FINALPROJ-main/Content/course1.css">
+</head>
+<body>
+    <div class="course-container">
+        <h1 class="title-course">COURSE 1</h1>
+        <div class="topics">
+            
+        </div>
+        
+    </div>
+
+    <div class="content">
+        <h1>rawr</h1>
+        <p></p>
+    </div>
+    
+</body>
+</html>
--- Content/courses-panel.php
+++ Content/courses-panel.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EDUCATIVO Courses</title>
     <link rel="stylesheet" href="/IT-ELECT-FINALPROJ-main/Content/courses-panel.css">
 </head>
 <body>
